34a83167d6aefef7c3e815626628394cf3b16a38
imattking <matt@imattking.com>
2022-09-23 19:22:12 -0400

    bug fixes

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,15 @@
 // Verify that web worker supoprt is enabled in browser
 /************************************************** */
 if (typeof(Worker) !== "undefined") {
-    console.log('Yes! Web worker support!');
+  console.log('Yes! Web worker support!');
 } else {
-    console.log('Sorry! No Web Worker support.');
+  console.log('Sorry! No Web Worker support.');
 }
 
 // Define HTML element variables
 /******************************* */
-const videoFrame = document.querySelector('#frame');
+const videoContainer = document.querySelector('#vid-container');
+const videoFrame = document.querySelector('#vid-frame');
 const scan = document.querySelector('#start');
 const scanResult = document.querySelector('#result');
 
@@ -17,44 +18,44 @@ const scanResult = document.querySelector('#result');
 
 // Start Scan using Device Camera
 scan.addEventListener('click', () => {
-    // toggle visibily to bring video preview into view
-    videoFrame.classList.toggle('hidden');
+  // toggle visibily to bring video preview into view
+  videoContainer.classList.toggle('hidden');
 
-    // initialize async/await
-    Quagga.init({
-        inputStream : {
-          name : "Live",
-          type : "LiveStream",
-          target: document.querySelector('#frame')  // highlight where in DOM to load preview
-        },
-        decoder : {
-          readers : ["upc_reader"] // specify which type of barcode reader
-        },
-      }, function(err) {
-          if (err) {
-              console.log(err);
-              return
+  // initialize async/await
+  Quagga.init({
+      inputStream : {
+        name : "Live",
+        type : "LiveStream",
+        target: document.querySelector('#vid-container')  // highlight where in DOM to load preview
+      },
+      decoder : {
+        readers : ["upc_reader"] // specify which type of barcode reader
+      },
+    }, function(err) {
+        if (err) {
+            console.log(err);
+            return
+        }
+        console.log("Initialization finished. Ready to start");
+
+        // Start scanning the bardcode
+        Quagga.start() 
+        //Quagga.onProcessed(callback);
+
+        // On detection process results of scan
+        Quagga.onDetected(function(result) {
+          if(result.codeResult) {
+              console.log("result", result.codeResult.code); // log to console as test
+              scanResult.innerText = `Result: ${result.codeResult.code}`; // place in DOM as preview
+              Quagga.stop();
+              videoFrame.classList.add('hidden'); // hide preview on completion
+          } else {
+              document.querySelector('#result').innerText = "not detected"; // display error text
+              videoFrame.classList.add('hidden'); // hide preivew on error
           }
-          console.log("Initialization finished. Ready to start");
-
-          // Start scanning the bardcode
-          Quagga.start() 
-          //Quagga.onProcessed(callback);
-
-          // On detection process results of scan
-          Quagga.onDetected(function(result) {
-            if(result.codeResult) {
-                console.log("result", result.codeResult.code); // log to console as test
-                scanResult.innerText = `Result: ${result.codeResult.code}`; // place in DOM as preview
-                Quagga.stop();
-                videoFrame.classList.add('hidden'); // hide preview on completion
-            } else {
-                document.querySelector('#result').innerText = "not detected"; // display error text
-                videoFrame.classList.add('hidden'); // hide preivew on error
-            }
-        });
-      })
-    });
+      });
+    })
+  });
 
 
 
